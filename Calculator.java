80eeccbfe11c10a953b7906649d30eedce4d0d8a
LPLLEU <38284033+lplleu@users.noreply.github.com>
2019-04-30 13:07:53 -0700

    Create Calculator.java
    a quick exercise for you to do
    30_April_2019

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+/*
+ask the user to give two int values. 
+if the 2 values are the same, display double their total.
+otherwise just display their total.
+*/
+
+public class Calculator{
+  public static void main(String[] args){
+
+
+  }
+}
