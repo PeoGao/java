0784d0fc967b76529aa5725c7e82bbed69ed8875
PeoGao <49922708+PeoGao@users.noreply.github.com>
2019-05-02 21:08:41 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,35 @@ if the 2 values are the same, display double their total.
 otherwise just display their total.
 */
 
-public class Calculator{
-  public static void main(String[] args){
+import java.util.Scanner;
+public class calculator {
+public static void main(String[] args){
 
+Scanner sc =new Scanner(System.in);
 
-  }
+int num1 =1;
+int num2=1;
+
+System.out.println("enter first number:");
+num1 = sc.nextInt();
+
+System.out.println("enter second number:");
+num2 = sc.nextInt();
+
+if(num2==num1){
+
+System.out.print(num1+num2*(num1+num2));
 }
+
+else{
+System.out.print(num1 + num2);
+}
+}
+}
+
+
+
+
+
+  
+
