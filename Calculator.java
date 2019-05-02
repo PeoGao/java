657ff62b15aae70ffb2eea3a7da4939942b303f9
LPLLEU <38284033+lplleu@users.noreply.github.com>
2019-05-02 21:13:24 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -10,8 +10,8 @@ public static void main(String[] args){
 
 Scanner sc =new Scanner(System.in);
 
-int num1 =1;
-int num2=1;
+int num1 =1; //why is it initialised? ke sa reye gore go molato
+int num2=1;  // why is it initialised? ke sa reye gore go molato
 
 System.out.println("enter first number:");
 num1 = sc.nextInt();
@@ -21,7 +21,7 @@ num2 = sc.nextInt();
 
 if(num2==num1){
 
-System.out.print(num1+num2*(num1+num2));
+System.out.print(num1+num2*(num1+num2)); // recheck calculation here
 }
 
 else{
