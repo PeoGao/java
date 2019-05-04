9f094751e6810dc863b650c31414c07d5e34b6f7
LPLLEU <38284033+lplleu@users.noreply.github.com>
2019-05-04 11:42:32 +0200

    Create teenagerCheck.java

---
+++ teenagerCheck.java
@@ -0,0 +1,15 @@
+/*
+
+you are given ages of two individuals, check whether any of them is a teenager.
+(a teenager has an age of between 13 and 19 inclusive).
+
+if any of the two individuals is a teenager, display "teenager found", otherwise display "no teenager found"
+
+*/
+
+import java.util.Scanner;
+public class calculator {
+  public static void teenagerCheck(String[] args){
+
+  }
+}
